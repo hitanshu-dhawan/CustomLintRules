8a3c97b82ddd1f2e72e59935e821eb17803fbea2
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-01-30 08:34:23 +0530

    BaseActivity

---
+++ app/src/main/java/com/hitanshudhawan/customlintrules/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.hitanshudhawan.customlintrules
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
--- lint-rules/src/main/java/com/hitanshudhawan/lint_rules/registry/IssueRegistry.kt
+++ lint-rules/src/main/java/com/hitanshudhawan/lint_rules/registry/IssueRegistry.kt
@@ -1,8 +1,10 @@
-package com.hitanshudhawan.lint_rules
+package com.hitanshudhawan.lint_rules.registry
 
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
+import com.hitanshudhawan.lint_rules.MyLogDetector
+import com.hitanshudhawan.lint_rules.MyTextViewDetector
 
 class IssueRegistry : IssueRegistry() {
 
